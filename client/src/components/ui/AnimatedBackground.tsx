cfc351911fab84075c53f3cefb1ae0506ee6fb99
rimo02 <ghoshrimo02@gmail.com>
2025-04-25 00:37:54 +0530

    error fix

--- client/src/components/ui/AnimatedBackground.tsx
+++ client/src/components/ui/AnimatedBackground.tsx
@@ -1,9 +1,15 @@
-const StarryBackground = ({ children }) => {
+import React, { ReactNode } from "react";
+
+interface StarryBackgroundProps {
+  children: ReactNode;
+}
+
+const StarryBackground: React.FC<StarryBackgroundProps> = ({ children }) => {
   return (
     <>
       {children}
     </>
-  )
+  );
 };
 
 export default StarryBackground;
